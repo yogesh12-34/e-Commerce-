45272778b4d1bf37ac9195c240a45b94d662eed6
Vipin-Sheoran <sheoranvipin2910@gmail.com>
2023-11-08 14:22:32 +0530

    fixed = sign missing

--- src/features/auth/authAPI.js
+++ src/features/auth/authAPI.js
@@ -15,7 +15,7 @@ export function checkUser(loginInfo) {
   return new Promise(async (resolve,reject) =>{
     const email=loginInfo.email
     const password=loginInfo.password
-    const response = await fetch(`http://localhost:8080/users?email`+email)
+    const response = await fetch(`http://localhost:8080/users?email=`+email)
     const data = await response.json()
     console.log({data})
     if(data.length){
