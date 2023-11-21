36653f5f08aa494d671c30c6babbe124109d45cb
yogesh12-34 <eche19058@rgipt.ac.in>
2023-11-21 19:53:49 +0530

    asas

--- src/features/user/components/userProfile.js
+++ src/features/user/components/userProfile.js
@@ -12,24 +12,31 @@ export default function UserProfile() {
 
   const dispatch = useDispatch();
   const user =useSelector(selectUserInfo)
+  const [selectedEditIndex,setSelectedEditIndex]= useState(-1)
   const {
     register,
     handleSubmit,
     reset,
     watch,
     formState: { errors },} = useForm();
-  const handleEdit =( )=>{
+  const handleEdit =( addressUpdate,index)=>{
+    const newUser={...user,addresses:[...user.addresses]}
+    newUser.addresses.splice(index,1,addressUpdate);
+    dispatch(updateUserAsync(newUser))
+    setSelectedEditIndex(-1)
+   
   };
   const handleRemove=(e, index)=>{
      const newUser={...user,addresses:[...user.addresses]}
      newUser.addresses.splice(index,1);
-     dispatch(updateUserAsync(newUser))
+     dispatch(updateUserAsync(newUser));
+
 
   }
 
   return (
     <div>
-      <div >
+      < >
       <div>
     <div className="mx-auto mt-12 bg-white max-w-7xl px-4 sm:px-6 lg:px-8"> 
     
@@ -46,11 +53,10 @@ export default function UserProfile() {
   <p className="mt-0.5 text-sm text-gray-500"> Your Address:</p>
   {user.addresses.map((address, index)=>(
     <div>
-    <form className="bg-white px-5 py-12 mt-12" noValidate onSubmit={handleSubmit((data)=>{
-            dispatch(updateUserAsync({...user,addresses:[...user.addresses,data]})
-            );
+  {selectedEditIndex=== index ?<form className="bg-white px-5 py-12 mt-12" noValidate onSubmit={handleSubmit((data)=>{
+            handleEdit(data,index)
             reset();
-          })}>
+          })}>:null}
           
         <div className="space-y-12">
         <div className="border-b border-gray-900/10 pb-12">
@@ -66,6 +72,7 @@ export default function UserProfile() {
                 <input
                   type="text"
                   {...register(`name`,{required:`name is required`})}
+                  value={address.name}
                   id="name"
                   
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -83,6 +90,7 @@ export default function UserProfile() {
                 <input
                   id="email"
                   {...register(`email`,{required:`email is required`})}
+                  value={address.email}
                   type="email"
                  
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -98,6 +106,7 @@ export default function UserProfile() {
               <input
                   id="phone"
                   {...register(`phone`,{required:`phone is required`})}
+                  value={address.phone}
                   type="tel"
                  
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -114,6 +123,7 @@ export default function UserProfile() {
                 <input
                   type="text"
                   {...register(`street`,{required:`street is required`})}
+                  value={address.street}
                   id="street-address"
                  
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -129,6 +139,7 @@ export default function UserProfile() {
                 <input
                   type="text"
                   {...register(`city`,{required:`city is required`})}
+                  value={address.city}
                   id="city"
                  
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -144,7 +155,8 @@ export default function UserProfile() {
                 <input
                   type="text"
                   {...register(`state`,{required:`state is required`})}
-                  id="region"
+                  value={address.state}
+                  id="state"
                   
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
@@ -159,6 +171,7 @@ export default function UserProfile() {
                 <input
                   type="text"
                   {...register(`pinCode`,{required:`pinCode is required`})}
+                  value={address.pinCode}
                   id="pinCode"
                 
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -175,7 +188,7 @@ export default function UserProfile() {
           type="submit"
           className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
-          Add Address
+          Edit Address
         </button>
       </div>
 
@@ -203,7 +216,7 @@ export default function UserProfile() {
           </div>
           <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
           <button
-                  onClick={e=>handleEdit(e,index)}
+                  onClick={e=>setSelectedEditIndex(index)}
                     type="button"
                     className="font-medium text-indigo-600 hover:text-indigo-500"
                   >
@@ -225,7 +238,7 @@ export default function UserProfile() {
         </div>
   </form>
 </div>
-))}
+))};
 </div>
 </div>
         
